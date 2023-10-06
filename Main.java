65f60eb8c11b17c4186a60b030cc11e28ec03049
isalunbel <142603867+isalunbel@users.noreply.github.com>
2023-10-06 19:05:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = Calculator.instance.get();
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
 
-        int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1, 1);
-        int c;
-        try {
-            c = calc.divide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
     }
 }
